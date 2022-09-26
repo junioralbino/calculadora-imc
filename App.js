b0810a9c575d6b37feefe1d6af3613cf44cc1d2e
Suporte <junioralbino741@gmail.com>
2022-09-26 11:52:21 -0300

    criando os components de inputs

--- App.js
+++ App.js
@@ -1,19 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, Button, Text, View } from 'react-native';
 
-import { Button } from 'react-native-paper';
+// import {  } from 'react-native-paper';
 
 export default function App() {
+  const calcularIMC = () => {
+    const peso = 94;
+    const altura = 1.83;
+
+    const imc = peso / (altura * altura);
+
+    alert(imc);
+  }
+
+
+  const imc = 25;
+  const legenda = 'Normal';
+
   return (
     <View style={styles.app}>
-      <Text>Criando novo texto</Text>
+    <StatusBar style="auto" />
+      <Text style={styles.legenda}>Seu IMC</Text>
+      <View>
+        <Text style={styles.resultado}>{imc}</Text>
+        <Text style={styles.diagnostico}>{legenda}</Text>
+      </View>
 
+      <View>
+        <TextInput style={styles.peso}/>
+        <TextInput style={styles.altura}/>
+        <Button title='Calcular' onPress={calcularIMC}/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   app: {
-   //
+   padding: 25,
   },
+  legenda: {
+    textAlign: "center",
+    fontWeight: 'bold'
+  },
+  resultado: {
+     textAlign: 'center',
+     fontSize: 22,
+     fontWeight: 'bold'
+  },
+  diagnostico: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: 'bold'
+  },
+  peso: {
+    borderColor: '#000',
+    borderWidth: 1
+  },
+  altura: {
+    borderColor: '#000',
+    borderWidth: 1
+  }
 });
